586016f8183c36f8cdd4d5f93d39291ea7602ded
Achalogy <achalogy@gmail.com>
2026-02-19 10:36:16 -0500

    start springboot

---
+++ maki/src/main/java/com/maki/maki/MakiApplication.java
@@ -0,0 +1,13 @@
+package com.maki.maki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakiApplication.class, args);
+	}
+
+}
