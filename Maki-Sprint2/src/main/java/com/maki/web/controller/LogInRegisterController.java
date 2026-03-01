cd46291ab2e6ca7cb97aef18d7297a6ade6548b1
JohnWilliams2050 <johnwilliams2050@hotmail.com>
2026-03-01 00:43:13 -0500

    Updating the project with new features and improvements

---
+++ Maki-Sprint2/src/main/java/com/maki/web/controller/LogInRegisterController.java
@@ -0,0 +1,22 @@
+package main.java.com.maki.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.maki.web.service.MenuService;
+
+@Controller
+@RequestMapping("/LogIn")
+public class LogInRegisterController {
+    
+    @Autowired
+    MenuService menuService;
+
+    @GetMapping("/log-in")
+    public String mostrarMenuEnTarjetas() {
+        return "log-in";
+    }
+    
+}
