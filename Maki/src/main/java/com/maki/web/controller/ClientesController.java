f0a8e686ecab96b342ae81476be3665664111c16
Achalogy <achalogy@gmail.com>
2026-03-05 09:33:02 -0500

    fix algunos problemas con retornar el cliente

--- Maki/src/main/java/com/maki/web/controller/ClientesController.java
+++ Maki/src/main/java/com/maki/web/controller/ClientesController.java
@@ -11,9 +11,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -72,6 +69,7 @@ public class ClientesController {
             if(e instanceof EntityConstraintException) {
                 return "redirect:/client/sign-up?msg=\"" + e.getMessage() + "\"";
             }
+            System.out.println(e);
             return "redirect:/client/sign-up?msg=\"Error desconocido\"";
         }
     }
