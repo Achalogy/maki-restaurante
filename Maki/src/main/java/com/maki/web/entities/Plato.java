4c0f053e7692803f50c52677e360138caf384dfa
Achalogy <achalogy@gmail.com>
2026-03-07 18:55:41 -0500

    data loader y fix plato

--- Maki/src/main/java/com/maki/web/entities/Plato.java
+++ Maki/src/main/java/com/maki/web/entities/Plato.java
@@ -2,6 +2,8 @@ package com.maki.web.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -14,6 +16,7 @@ import lombok.Data;
 public class Plato {
     @Id
     @Column(name = "id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @Column(name = "nombre", length = 100, nullable = false)
