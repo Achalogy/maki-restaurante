1864ec87392d52c16bf5bb3c24e96b8c6b553c63
Achalogy <achalogy@gmail.com>
2026-03-07 18:07:43 -0500

    agregar @entity a las entidades

--- Maki/src/main/java/com/maki/web/entities/Plato.java
+++ Maki/src/main/java/com/maki/web/entities/Plato.java
@@ -1,16 +1,26 @@
 package com.maki.web.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Entity
 public class Plato {
+    @Id
     private Integer id;
+    
     private String nombre;
     private double precio;
     private String descripcion;
     private String urlImage;
     private boolean disponible;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 }
