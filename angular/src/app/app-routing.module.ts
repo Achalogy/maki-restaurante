cd63208058eb9ed07fef27df6f6e7dda9e04935b
alex01-07 <aponte_a@javeriana.edu.co>
2026-04-04 17:51:48 -0500

    feat: agregar formulario crear y editar operarios con datos quemados

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { PlateEditComponent } from './plates/plate-edit/plate-edit.component';
 import { PlateCreateComponent } from './plates/plate-create/plate-create.component';
 import { PlateViewComponent } from './plates/plate-view/plate-view.component';
 import { LandingPageComponent } from './landing/landing-page/landing-page.component';
+import { OperarioFormComponent } from './components/operario-form/operario-form.component';
 
 const routes: Routes = [
   { path: 'plate/crud', component: PlateTableComponent },
@@ -12,8 +13,14 @@ const routes: Routes = [
   { path: 'plate/create', component: PlateCreateComponent },
   { path: 'plate/:id', component: PlateViewComponent },
   { path: '', component: LandingPageComponent },
+
+  { path: 'operarios/crear', component: OperarioFormComponent },
+  { path: 'operarios/editar/:id', component: OperarioFormComponent },
+
   { path: '**', pathMatch: 'full', redirectTo:''}
+  
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
