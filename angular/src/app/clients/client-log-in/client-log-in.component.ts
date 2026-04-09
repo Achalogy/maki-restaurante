3e0aa0abdc8e216bf8c9dc14743ac7c38d87f98f
Achalogy <achalogy@gmail.com>
2026-04-09 10:06:19 -0500

    crear todos los nuevos componentes

---
+++ angular/src/app/clients/client-log-in/client-log-in.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-log-in',
+  templateUrl: './client-log-in.component.html',
+  styleUrls: ['./client-log-in.component.css']
+})
+export class ClientLogInComponent {
+
+}
