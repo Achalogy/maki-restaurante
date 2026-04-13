10e48ff9ef0dd105960c7ee25d8851e5d887fb9c
Achalogy <achalogy@gmail.com>
2026-04-12 20:18:41 -0500

    linkear form con modelo

--- angular/src/app/clients/client-log-in/client-log-in.component.ts
+++ angular/src/app/clients/client-log-in/client-log-in.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Client } from 'src/app/interfaces/client.interface';
 
 @Component({
   selector: 'app-client-log-in',
@@ -7,11 +8,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./client-log-in.component.css']
 })
 export class ClientLogInComponent {
+
+  email: string = "";
+  password: string = ""
+
   constructor(
     private router: Router
   ) { }
 
   navigateTo(url: string) {
     this.router.navigate([url])
+  }
+
+  onSubmit(): void {
+
   }
 }
--- angular/src/app/clients/client-sign-up/client-sign-up.component.ts
+++ angular/src/app/clients/client-sign-up/client-sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Client } from 'src/app/interfaces/client.interface';
 
 @Component({
   selector: 'app-client-sign-up',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./client-sign-up.component.css']
 })
 export class ClientSignUpComponent {
+
+  client: Partial<Client> = {};
+
+  constructor(
+      private router: Router
+    ) { }
+  
+    navigateTo(url: string) {
+      this.router.navigate([url])
+    }
 
 }
