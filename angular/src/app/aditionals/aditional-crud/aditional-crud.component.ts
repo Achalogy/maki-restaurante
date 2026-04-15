0f83a83938fc011a0065d34d4db5cd2d50707121
Achalogy <achalogy@gmail.com>
2026-04-15 11:32:53 -0500

    crear componentes y su ruta

---
+++ angular/src/app/aditionals/aditional-crud/aditional-crud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aditional-crud',
+  templateUrl: './aditional-crud.component.html',
+  styleUrls: ['./aditional-crud.component.css']
+})
+export class AditionalCrudComponent {
+
+}
