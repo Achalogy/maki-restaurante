6d76518a404245e16046ad3d8ef4a221f6108a52
Achalogy <achalogy@gmail.com>
2026-04-19 15:02:13 -0500

    cerrar sesión, cambiar texto nav bar y agregar boton carrito

--- angular/src/app/components/header-menu/header-menu.component.ts
+++ angular/src/app/components/header-menu/header-menu.component.ts
@@ -7,10 +7,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./header-menu.component.css']
 })
 export class HeaderMenuComponent {
+  public loggedAs: "client" | "operator" | null = null;
+  public loggedId: string |  null = null;
 
   constructor(
     private router: Router
   ) {}
+
+  ngOnInit() {
+    this.loggedAs = window.localStorage.getItem("loggedAs") as any
+    this.loggedId = window.localStorage.getItem("id") as any
+  }
+
+  goToProfile() {
+    if(this.loggedAs == "client")
+      this.router.navigate([`/client/${this.loggedId}`])
+    else {
+      // TODO: OPERATOR
+    }
+  }
 
   navigateTo(url: string) {
     this.router.navigate([url])
