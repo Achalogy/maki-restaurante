22411296cb7445cf5e2bca26a2a5119db86fc825
Achalogy <achalogy@gmail.com>
2026-04-19 14:45:08 -0500

    mover archivos para orden

--- angular/src/app/data/categorias.data.ts
+++
@@ -1,9 +0,0 @@
-import { Category } from "../interfaces/category.interface";
-
-export const CATEGORIES: Category[] = [
-  { id: 1, name: 'Entradas' },
-  { id: 2, name: 'Platos fuertes' },
-  { id: 3, name: 'Sushi' },
-  { id: 4, name: 'Postres' },
-  { id: 5, name: 'Bebidas' }
-];
