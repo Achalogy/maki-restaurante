044f0300ee4b2809b767586a1016573dd316f4d2
Juan_vargas <johnwilliams2050@hotmail.com>
2026-04-28 01:27:30 +0545

    agregando el campo de orders al cliente para mostrar sus ordenes en su perfil

--- angular/src/app/pages/clients/client-profile/client-profile.component.ts
+++ angular/src/app/pages/clients/client-profile/client-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Client } from 'src/app/interfaces/client.interface';
+import { PurchaseOrderDetails } from 'src/app/interfaces/order-details.interface';
 import { ClientService } from 'src/app/service/data/client.service';
 import { OrderDetailsService } from 'src/app/service/data/order-details.service';
 
@@ -11,6 +12,11 @@ import { OrderDetailsService } from 'src/app/service/data/order-details.service'
 })
 export class ClientProfileComponent implements OnInit {
   client: Client | undefined;
+
+  orderItems: PurchaseOrderDetails[] = [];
+  get orderInfo() {
+    return this.orderItems[0]?.order ?? null;
+  }
 
   constructor(
     private clientService: ClientService,
