30a5090cc4e5c94516b05d335e5b461c45d5a9dc
Achalogy <achalogy@gmail.com>
2026-05-02 14:46:23 -0500

    Limpiar algunas cosas

--- Maki/src/main/java/com/maki/web/controller/PurchaseOrderController.java
+++ Maki/src/main/java/com/maki/web/controller/PurchaseOrderController.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -90,7 +89,7 @@ public class PurchaseOrderController {
                     newDelivery.setBusy(false);
                     deliveryService.update(newDelivery);
                 }
-                
+
                 if(oldDelivery != null) {
                     oldDelivery.setBusy(false);
                     deliveryService.update(oldDelivery);
--- Maki/src/main/java/com/maki/web/service/OperatorService.java
+++ Maki/src/main/java/com/maki/web/service/OperatorService.java
@@ -2,7 +2,6 @@ package com.maki.web.service;
 
 import com.maki.web.entities.Operator;
 import com.maki.web.exception.InvalidCredentialsException;
-import com.maki.web.exception.EntityConstraintException;
 import com.maki.web.exception.EntityNotFoundException;
 
 public interface OperatorService extends ServiceInterface<Operator> {
