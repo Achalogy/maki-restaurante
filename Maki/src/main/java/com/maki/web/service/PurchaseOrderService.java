828ae66a568f460fdf1936fe9d10a16bd5733c3d
Achalogy <achalogy@gmail.com>
2026-05-05 10:06:41 -0500

    Probar el metodo setDelivery en el service de purchase order

--- Maki/src/main/java/com/maki/web/service/PurchaseOrderService.java
+++ Maki/src/main/java/com/maki/web/service/PurchaseOrderService.java
@@ -2,12 +2,15 @@ package com.maki.web.service;
 
 import java.util.List;
 
+import com.maki.web.entities.Delivery;
 import com.maki.web.entities.PlateWithAdditionals;
 import com.maki.web.entities.PurchaseOrder;
+import com.maki.web.exception.EntityConstraintException;
 
 
 
 public interface PurchaseOrderService extends ServiceInterface<PurchaseOrder> {
   PurchaseOrder createPurchaseOrderFromcart(Long id, List<PlateWithAdditionals> plates);
   List<PurchaseOrder> selectNotCompleted();
+  PurchaseOrder setDelivery(Long id, Delivery delivery) throws EntityConstraintException;
 }
