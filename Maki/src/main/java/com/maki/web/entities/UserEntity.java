1babff535811d881ad6af10fa7f36bce57a0ed9f
Achalogy <achalogy@gmail.com>
2026-05-18 22:01:56 -0500

    mapeos a UserEntity

--- Maki/src/main/java/com/maki/web/entities/UserEntity.java
+++ Maki/src/main/java/com/maki/web/entities/UserEntity.java
@@ -8,14 +8,9 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import com.maki.web.entities.Role;
 
 @Entity
 @Table(name = "users")
